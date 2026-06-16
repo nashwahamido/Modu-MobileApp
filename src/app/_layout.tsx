d4e273e0e23e3d59c288e722dd57d9f1f5145806
Ge You <youge071@gmail.com>
2026-06-16 10:43:44 +0100

    move App.tsx to app folder, using expo router structure
    By using expo router:
    - Every .tsx file in app/ automatically becomes a route
    - index.tsx maps to the root path (/)
    - _layout.tsx is not a page — it wraps its folder's routes and defines their navigation structure (tabs, stack, etc.)
    
    for detailed rules, check out: https://docs.expo.dev/router/basics/core-concepts/

---
+++ src/app/_layout.tsx
@@ -0,0 +1,16 @@
+// This is the root router, where initialization code sits
+// controlling dark/light theme
+
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import 'react-native-reanimated';
+
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack screenOptions={{ headerShown: false }} />
+      <StatusBar style="auto" hidden />
+    </GestureHandlerRootView>
+  );
+}
